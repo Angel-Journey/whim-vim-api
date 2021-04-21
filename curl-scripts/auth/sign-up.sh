ef08c9d96f1e32c50c3a6f3daf45ff566d3834d9
Angel-Journey <angel.journey7@gmail.com>
2021-04-21 15:48:20 -0500

    Submit progress - setting up mongo and heroku

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -3,7 +3,7 @@
 # EMAIL="test1@testmail.com" PASSWORD="b" sh curl-scripts/auth/sign-up.sh
 # ID="6080809ff64881fb446b5119"
 
-API="http://localhost:4741"
+API="https://whim-vim-api.herokuapp.com"
 URL_PATH="/sign-up"
 
 curl "${API}${URL_PATH}" \
