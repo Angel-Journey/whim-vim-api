251a5061715d2cc95b2a7b7fd6470d7e29802d92
Angel-Journey <angel.journey7@gmail.com>
2021-04-26 20:40:30 -0500

    Submit updates to README.md and curl-scripts

--- curl-scripts/whims/destroy.sh
+++ curl-scripts/whims/destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ID="60817a2b9a15fb0015f0da1e" TOKEN="ba3a51a87092db5b241a3a5ae22bcb74" sh curl-scripts/whims/destroy.sh
+# ID="608310ca33c2a20015fd8f2c" TOKEN="9c7a4027d06ac4bc8719f71a49071a06" sh curl-scripts/whims/destroy.sh
 
 API="https://whim-vim-api.herokuapp.com"
 URL_PATH="/whims"
--- curl-scripts/whims/update.sh
+++ curl-scripts/whims/update.sh
@@ -3,7 +3,7 @@
 # TOKEN="1286b06e8f13c420556863ba708f2229" TITLE="Soccer" TEXT="Playing Keeper" sh curl-scripts/whims/create.sh
 # ID="60805c185e823af86f152707"
 
-# ID="60817a2b9a15fb0015f0da1e" TOKEN="ba3a51a87092db5b241a3a5ae22bcb74" TEXT="Indoor" sh curl-scripts/whims/update.sh
+# ID="608310a433c2a20015fd8f2b" TOKEN="9c7a4027d06ac4bc8719f71a49071a06" TITLE="TESTING" TEXT="Indoor" sh curl-scripts/whims/update.sh
 
 API="https://whim-vim-api.herokuapp.com"
 URL_PATH="/whims"
